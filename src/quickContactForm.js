89fc71db708e68091e4bb7e43c6a993626e4f200
Tyrantz <raymanv3@gmail.com>
2016-03-06 15:39:07 +0000

    adds contact form component

---
+++ src/quickContactForm.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+var QuickContactForm = React.createClass({
+  render() {
+    return (
+      <form className="form-horizontal">
+        <div className="form-group">
+          <label for="inputEmail3" className="col-sm-2 control-label">Email</label>
+          <div className="col-sm-10">
+            <input type="email" className="form-control" id="inputEmail3" placeholder="Email" />
+          </div>
+        </div>
+        <div className="form-group">
+          <label for="inputPassword3" className="col-sm-2 control-label">Password</label>
+          <div className="col-sm-10">
+            <input type="password" className="form-control" id="inputPassword3" placeholder="Password" />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="col-sm-offset-2 col-sm-10">
+            <div className="checkbox">
+              <label>
+                <input type="checkbox" /> Remember me
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="col-sm-offset-2 col-sm-10">
+            <button type="submit" className="btn btn-default">Sign in</button>
+          </div>
+        </div>
+      </form>
+    );
+  }
+});
+
+module.exports = QuickContactForm;

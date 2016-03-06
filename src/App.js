7e5f4b52fbe728ea530bebfd7fe951a548575158
Tyrantz <raymanv3@gmail.com>
2016-03-06 15:40:44 +0000

    adds basic form for quick contact

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // var React = require("react");
 import React from "react";
 import NavBar from "./navbar";
+import QuickContactForm from "./quickContactForm.js";
 require("bootstrap");
 require("bootstrap-webpack");
 require("./style.scss");
@@ -34,6 +35,7 @@ var Header = React.createClass({
     return (
       <div className="header-title">
         <h1>Web and Mobile Developer</h1>
+        <h3>Reach Me</h3>
       </div>
     );
 }
@@ -45,7 +47,8 @@ var QuickContact = React.createClass({
       <div className="quick-contact-container">
         <div className="quick-contact-box">
           <Header />
-          <div className="quick-contact-form">Web and Mobile Developer</div>
+          {/* <div className="quick-contact-form">Web and Mobile Developer</div> */}
+          <QuickContactForm />
         </div>
       </div>
     );

87b9f6e1fd550846ba3ed847e1625275c1127062
Tyrantz <raymanv3@gmail.com>
2016-04-27 11:31:29 +0000

    switches to mobile first css

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 export default () => {
     return(
-      <header className="">
+      <header>
         <div className="landing-page-curtain img-responsive"></div>
-        <h1>Fanaticus <small>Full Stack Web Developer</small></h1>
-        <div className="landing-page-buttons">
-          <a href="#" className="button-contact-me">Contact Me</a>
-          <a href="#" className="button-portfolio">Portfolio</a>
+        <div className="landing-page-content">
+          <h1>Fanaticus <small>Full Stack Web Developer</small></h1>
+          <div className="landing-page-buttons">
+            <a href="#" className="button-contact-me">Contact Me</a>
+            <a href="#" className="button-portfolio">Portfolio</a>
+          </div>
         </div>
       </header>
     );

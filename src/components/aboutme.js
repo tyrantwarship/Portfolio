abb28e114d199dc99b1b20aff6c82baf44c3ad58
Tyrantz <raymanv3@gmail.com>
2016-04-27 13:53:07 +0000

    mobile first aboutme section

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -4,10 +4,8 @@ export default () => {
     return (
       <div className="text-center row" id="aboutme">
         <img src="http://placehold.it/250x250" className="img-circle about-me-picture text-center" />
-        <h1 className="about-me-header">Who am I ?</h1>
-        <p className="about-me-paragraph col-sm-offset-2 col-sm-8 text-left">Adipisicing pariatur similique voluptas earum modi illum rem velit quidem deserunt illum. Veritatis voluptatem ex doloribus fugit sequi, tenetur, nobis! Tenetur nesciunt temporibus doloribus ea rem impedit delectus necessitatibus officiis? Ipsum quae veniam itaque dolorem aliquid ea itaque! Consectetur iure sapiente id et iusto quasi illum officiis adipisci, temporibus. Iusto laudantium ipsa similique temporibus culpa ducimus? Quis qui omnis asperiores.</p>
-        <p className="about-me-paragraph col-sm-offset-2 col-sm-8 text-left">Adipisicing pariatur similique voluptas earum modi illum rem velit quidem deserunt illum. Veritatis voluptatem ex doloribus fugit sequi, tenetur, nobis! Tenetur nesciunt temporibus doloribus ea rem impedit delectus necessitatibus officiis? Ipsum quae veniam itaque dolorem aliquid ea itaque! Consectetur iure sapiente id et iusto quasi illum officiis adipisci, temporibus. Iusto laudantium ipsa similique temporibus culpa ducimus? Quis qui omnis asperiores.</p>
-        <p className="about-me-paragraph col-sm-offset-2 col-sm-8 text-left">Adipisicing pariatur similique voluptas earum modi illum rem velit quidem deserunt illum. Veritatis voluptatem ex doloribus fugit sequi, tenetur, nobis! Tenetur nesciunt temporibus doloribus ea rem impedit delectus necessitatibus officiis? Ipsum quae veniam itaque dolorem aliquid ea itaque! Consectetur iure sapiente id et iusto quasi illum officiis adipisci, temporibus. Iusto laudantium ipsa similique temporibus culpa ducimus? Quis qui omnis asperiores.</p>
+        <h1 className="about-me-header">Who am I ? <small>Glad you asked</small></h1>
+        <p className="about-me-paragraph col-sm-offset-2 col-sm-8 text-left">Hi, my name is Felipe "Fanaticus" Cantu. I am a self-taught Web and Mobile Developer, who loves learning new technologies and am always looking to take on new, exciting, and challenging projects.</p>
       </div>
     );
 }

421e3f331c8f0adf84b72dfba58f580c68306277
Tyrantz <tyrantwarzhip@gmail.com>
2016-06-04 04:32:57 -0500

    updates about me section

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -5,7 +5,7 @@ export default () => {
       <div className="text-center row" id="aboutme">
         <img src="http://placehold.it/250x250" className="img-circle about-me-picture text-center" />
         <h1 className="about-me-header">Who am I ? <small>Glad you asked</small></h1>
-        <p className="about-me-paragraph col-sm-offset-2 col-sm-8 text-left">Hi, my name is Felipe "Fanaticus" Cantu. I am a self-taught Web and Mobile Developer, who loves learning new technologies and am always looking to take on new, exciting, and challenging projects.</p>
+        <p className="about-me-paragraph col-sm-offset-2 col-sm-8 text-left">Hi, my name is Felipe "Fanaticus" Cantu. I am a self-taught Web and Mobile Developer, who loves learning new technologies and am always looking forward to take on new, exciting, and challenging projects.</p>
       </div>
     );
 }

b43f1968d6b3f6dde9e14bf2f37a4259ba45336b
Tyrantz <tyrantwarzhip@gmail.com>
2016-06-06 04:54:00 -0500

    removes the offset to center stuff and adds margin

--- src/components/contactme.js
+++ src/components/contactme.js
@@ -14,7 +14,7 @@ export default () => {
     <div className="row contactme-area text-center" id="contactme">
       <h1>Contact Me</h1>
       <h2>Got an Idea? Lets make it happen!</h2>
-      <ul className="list-inline col-sm-offset-2 badges-list">
+      <ul className="list-inline badges-list">
         {
           contactmethods.map((value, index) =>
             <li className="col-xs-3 contactme-badge" key={index}>

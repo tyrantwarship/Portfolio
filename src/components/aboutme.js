da00a9b7be8bad9e7d58d0c3c9f52a81ca75c8b0
Tyrantz <tyrantwarzhip@gmail.com>
2016-12-11 01:04:09 -0600

    updates bio

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -5,7 +5,7 @@ export default () => {
       <div className="text-center row" id="aboutme">
         <img src="http://placehold.it/250x250" className="img-circle about-me-picture text-center" />
         <h1 className="about-me-header">Who am I ? <small>Glad you asked</small></h1>
-        <p className="about-me-paragraph col-sm-offset-2 col-sm-8 text-left">Hi, my name is Felipe "Fanaticus" Cantu. I am a self-taught Web and Mobile Developer, who loves learning new technologies and am always looking forward to take on new, exciting, and challenging projects.</p>
+        <p className="about-me-paragraph col-sm-offset-2 col-sm-8 text-left"><span className="about-hi">Hi!</span> I am Felipe Romo; Web and Mobile Developer specializing on the React ecosystem! I am an avid developer with a keen interest for all things technology. I love to keep up with the latest trends in web development; whether it be new frameworks or programming paradigms. I always aim to write clean, concise, and elegant applications; that are beautiful and work accross all popular browsers.</p>
       </div>
     );
 }

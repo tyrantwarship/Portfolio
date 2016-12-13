4255f42f199e1d84991505a9c9bcefc744ff29d1
Tyrantz <tyrantwarzhip@gmail.com>
2016-12-12 22:11:23 -0600

    footer fix

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default () => {
     return (
       <div className="footer-area row">
-        <p className="pull-left text-muted">Felipe Cantu</p>
+        <p className="pull-left text-muted">Felipe Romo</p>
         <ul className="list-inline pull-right text-muted">
           <li><a href="#aboutme" className="text-muted">About Me</a></li>
           <li><a href="#skillset" className="text-muted">Skill Set</a></li>

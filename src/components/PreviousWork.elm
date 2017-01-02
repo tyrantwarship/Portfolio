f20557824c95ab45b8bcbbb3b6fca1cc2425be2b
Tyrantz <tyrantwarzhip@gmail.com>
2017-01-02 13:54:54 -0600

    carousel for 1 slide

--- src/components/PreviousWork.elm
+++ src/components/PreviousWork.elm
@@ -1,12 +1,13 @@
 module PreviousWork exposing (main)
 
-import Html exposing (div, text, ol, li)
-import Html.Attributes exposing (class, id, attribute)
+import Html exposing (div, text, ol, li, a, img, h3, p, span)
+import Html.Attributes exposing (class, id, attribute, target, href, alt, src)
 
 
 -- import Html.Events
 
 
+main : Html.Html msg
 main =
     div [ class "container" ]
         [ div
@@ -14,28 +15,87 @@ main =
             , class "carousel slide"
             , attribute "data-ride" "carousel"
             ]
-            [ ol [ class "carousel-indicators" ]
-                [ li
+            [ carouselIndicators
+            , carouselInner
+            , carouselControlLeft
+            , carouselControlRight
+            ]
+        ]
+
+
+carouselIndicators : Html.Html msg
+carouselIndicators =
+    ol [ class "carousel-indicators" ] <|
+        List.map
+            (\( className, slide ) ->
+                li
                     [ attribute "data-target" "#myCarousel"
-                    , attribute "data-slide-to" "0"
-                    , class "active"
+                    , attribute "data-slide-to" <| toString slide
+                    , class className
                     ]
                     []
-                , li
-                    [ attribute "data-target" "#myCarousel"
-                    , attribute "data-slide-to" "1"
-                    ]
-                    []
-                , li
-                    [ attribute "data-target" "#myCarousel"
-                    , attribute "data-slide-to" "4"
-                    ]
-                    []
-                , li
-                    [ attribute "data-target" "#myCarousel"
-                    , attribute "data-slide-to" "3"
-                    ]
-                    []
+            )
+            [ ( "active", 0 ), ( "", 1 ), ( "", 2 ), ( "", 3 ) ]
+
+
+carouselInner : Html.Html msg
+carouselInner =
+    div
+        [ class "carousel-inner"
+        , attribute "role" "listbox"
+        ]
+        [ carouselItem ]
+
+
+carouselItem : Html.Html msg
+carouselItem =
+    div [ class "item active simon-item" ]
+        [ a
+            [ target "_blank"
+            , href "http://tyrantwarship.github.io/FCC-SimonSays-Zipline"
+            ]
+            [ img
+                [ alt "simon-project"
+                , src "https://raw.githubusercontent.com/tyrantwarship/FCC-SimonSays-Zipline/master/demo-first.png"
+                ]
+                []
+            , div [ class "carousel-caption caption-description" ]
+                [ h3 [] [ text "Simon Says" ]
+                , p [] [ text """Web application based on the timeless game, "Simon Says". Built with React and Sass.""" ]
                 ]
             ]
+        ]
+
+
+carouselControlLeft : Html.Html msg
+carouselControlLeft =
+    a
+        [ class "left carousel-control"
+        , href "#myCarousel"
+        , attribute "role" "button"
+        , attribute "data-slide" "prev"
+        ]
+        [ span
+            [ class "glyphicon glyphicon-chevron-left"
+            , attribute "aria-hidden" "true"
+            ]
+            []
+        , span [ class "sr-only" ] [ text "Previous" ]
+        ]
+
+
+carouselControlRight : Html.Html msg
+carouselControlRight =
+    a
+        [ class "right carousel-control"
+        , href "#myCarousel"
+        , attribute "role" "button"
+        , attribute "data-slide" "next"
+        ]
+        [ span
+            [ class "glyphicon glyphicon-chevron-right"
+            , attribute "aria-hidden" "true"
+            ]
+            []
+        , span [ class "sr-only" ] [ text "Next" ]
         ]
